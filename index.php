e998dfce0ac4907c0a35654b2a2c4b87b131c5c8
De Roover Justin <justinderoover@gmail.com>
2023-11-27 16:13:42 +0100

    add footer

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <?php
-echo('Hekko World');
+echo ('Hekko World');
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Document</title>
 </head>
+
 <body>
     <?php include("header.php"); ?>
     <?php include("footer.php"); ?>
 </body>
+
 </html>
