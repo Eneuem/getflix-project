2b1c1b17f7ead2df0b712a856715562d2a5cd5b6
Eneuem <nemozor@gmail.com>
2023-11-27 16:31:55 +0100

    logo

--- index.php
+++ index.php
@@ -7,7 +7,8 @@ echo('Hekko World');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
